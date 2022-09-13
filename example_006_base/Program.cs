666ec06f85dd29326fe2295f3c3910a007b4d204
Dr.Ddiezel93 <ghek93@mail.ru>
2022-09-14 00:12:37 +0300

    006

---
+++ example_006_base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 2;
+int b = 4;
+int c = 1;
+int d = 9;
+int f = 0;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (f > max) max = f;
+
+Console.Write ("max = ");
+Console.WriteLine (max);
